55d1ddd6dcb101802bcf39699055e9d87a06918d
Permatick <168349556+Permatick@users.noreply.github.com>
2025-04-27 14:31:23 +0300

    Aaaaaaaaaa

--- peer_discovery.py
+++ peer_discovery.py
@@ -1,40 +1,37 @@
 import socket
 import json
-import os
 import time
-import datetime
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind(("",6000))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("", 6000))
+sock.settimeout(2)
 
-peers = {}
+try:
+    with open("peers.json", "r") as f:
+        peers = json.load(f)
+except:
+    peers = {}
 
-print("Peer Discovery has been started. I am listening all the broadcasts...\n")
+print("Peer Discovery started, listening for broadcasts...")
 
 while True:
     try:
-        print("Bekleniyor...")
-        data,addr = sock.recvfrom(1024)
-        print("Veri alındı:", addr)
-        
-        message = data.decode()
-        info = json.loads(message)
-        
-        username = info["username"]
+        data, addr = sock.recvfrom(1024)
+        message = json.loads(data.decode())
+        username = message["username"]
         ip = addr[0]
         timestamp = time.time()
-        
-        if ip not in peers:
-            print(f"{username} ({ip}) is online ")
 
-        peers[ip] = {
-            "username" : username,
-            "last_seen" : timestamp
-        }
-        
+        if ip in peers:
+            peers[ip]["last_seen"] = timestamp
+        else:
+            peers[ip] = {"username": username, "last_seen": timestamp}
+            print(f"{username} ({ip}) is online!")
+
         with open("peers.json", "w") as f:
             json.dump(peers, f, indent=4)
-    
-    except Exception as e:
-        print("There is an error...")
+
+    except socket.timeout:
         continue
+    except Exception as e:
+        print(f"Error: {e}")
