13372743ffdcccc74284b3418303a44cd52ad47f
Yağız Süzen <168354802+Yagizsuzen@users.noreply.github.com>
2025-04-30 21:56:18 +0300

    Update service_announcer.py

--- service_announcer.py
+++ service_announcer.py
@@ -1,18 +1,32 @@
 import socket
 import json
-import time
-
-with open("user_info.json", "r") as f:
-    user_info = json.load(f)
-
-username = user_info["username"]
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-message = json.dumps({"username": username})
+import os
 
 while True:
-    sock.sendto(message.encode(), ("255.255.255.255", 6000))
-    print(f"Broadcast sent: {username}")
-    time.sleep(8)
+    username = input("Please enter your username: ").strip()
+    if username:
+        break
+    print("Username can not be blank!")
+
+try:
+    ip_address = socket.gethostbyname(socket.gethostname())
+    
+    if ip_address.startswith("127."):
+        s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+
+except Exception as e:
+    print("IP address could not find.")
+    ip_address = "0.0.0.0"
+
+user_info = {
+    "username" : username,
+    "ip" : ip_address
+}
+
+with open("user_info.json","w") as f:
+    json.dump(user_info,f, indent=4)
+
+print("User info saved succesfully.")
